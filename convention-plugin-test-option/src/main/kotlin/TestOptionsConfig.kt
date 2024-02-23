efd56c7755dc3dae15934e8490125e5909ec6dca
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2024-02-23 04:50:51 +0100

    Updated unit test options

--- convention-plugin-test-option/src/main/kotlin/TestOptionsConfig.kt
+++ convention-plugin-test-option/src/main/kotlin/TestOptionsConfig.kt
@@ -5,11 +5,17 @@ import org.gradle.kotlin.dsl.create
 fun TestOptions.configureTestOptions() {
     unitTests {
         all { test: org.gradle.api.tasks.testing.Test ->
-            test.testLogging.events = setOf(
-                org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
-                org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
-                org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-            )
+            test.testLogging {
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+                events = setOf(
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
+                    org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+                )
+            }
         }
     }
     animationsDisabled = true
