d85d4affd5f552709fd817ee8d0799f03a176cee
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2024-09-13 07:25:53 +0200

    Fixed iOS target

--- composeApp/src/iosMain/kotlin/Main.kt
+++ composeApp/src/iosMain/kotlin/Main.kt
@@ -3,6 +3,8 @@ import org.koin.core.Koin
 import platform.UIKit.UIViewController
 import siarhei.luskanau.pixabayeye.navigation.App
 
-fun mainViewController(koin: Koin): UIViewController = ComposeUIViewController {
+fun mainViewController(koin: Koin): UIViewController = ComposeUIViewController(
+    configure = { enforceStrictPlistSanityCheck = false }
+) {
     App(koin = koin)
 }
