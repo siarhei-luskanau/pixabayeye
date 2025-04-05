d0c1724ead8083305a2002f9daf3d6df55d5e896
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-04-05 08:32:45 +0200

    Enable Gradle configuration cache

--- ui/uiSearch/build.gradle.kts
+++ ui/uiSearch/build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     id("composeMultiplatformKspConvention")
     id("testOptionsConvention")
-    alias(libs.plugins.compose.screenshot)
 }
 
 android {
     namespace = "siarhei.luskanau.pixabayeye.ui.search"
     testOptions.configureTestOptions()
-    experimentalProperties["android.experimental.enableScreenshotTest"] = true
 }
 
 kotlin {
