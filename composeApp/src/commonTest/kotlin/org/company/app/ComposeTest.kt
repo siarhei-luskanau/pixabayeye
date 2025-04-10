2bc159113d9b945067c6c46a44d10c9459df48ca
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-04-10 15:56:24 +0200

    Fixed LifecycleOwner issue in common ComposeUiTest

--- composeApp/src/commonTest/kotlin/org/company/app/ComposeTest.kt
+++ composeApp/src/commonTest/kotlin/org/company/app/ComposeTest.kt
@@ -1,10 +1,15 @@
 package org.company.app
 
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import kotlin.test.Test
 import siarhei.luskanau.pixabayeye.KoinApp
 
@@ -14,12 +19,24 @@ class ComposeTest {
     @Test
     fun simpleCheck() = runComposeUiTest {
         setContent {
-            KoinApp()
+            // workaround "CompositionLocal LocalLifecycleOwner not present"
+            // https://youtrack.jetbrains.com/issue/CMP-7419/CompositionLocal-LocalLifecycleOwner-not-present-on-iosSimulatorArm64Test-task
+            CompositionLocalProvider(
+                LocalLifecycleOwner provides LocalLifecycleOwnerFake()
+            ) {
+                KoinApp()
+            }
         }
         val testInput = "test123abc"
         onNodeWithTag("search_text_field").apply {
             performTextInput(text = testInput)
         }
         onNodeWithTag("search_text_field").assertTextContains(value = testInput)
+    }
+}
+
+private class LocalLifecycleOwnerFake : LifecycleOwner {
+    override val lifecycle: Lifecycle = LifecycleRegistry(this).apply {
+        currentState = Lifecycle.State.RESUMED
     }
 }
