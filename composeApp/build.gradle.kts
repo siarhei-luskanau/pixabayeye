ddb36eb6d8f910ae94efd1e441f409d1bc5ccf90
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-11-20 19:49:20 +0100

    Bump dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,19 +9,6 @@ kotlin {
     androidLibrary.namespace = "siarhei.luskanau.pixabayeye.app"
     sourceSets {
         commonMain.dependencies {
-            implementation(libs.jetbrains.compose.animation)
-            implementation(libs.jetbrains.compose.animation.graphics)
-            implementation(libs.jetbrains.compose.components.resources)
-            implementation(libs.jetbrains.compose.foundation)
-            implementation(libs.jetbrains.compose.material.icons.extended)
-            implementation(libs.jetbrains.compose.material3)
-            implementation(libs.jetbrains.compose.material3.adaptive.navigation.suite)
-            implementation(libs.jetbrains.compose.runtime)
-            implementation(libs.jetbrains.compose.runtime.saveable)
-            implementation(libs.jetbrains.compose.ui)
-            implementation(libs.jetbrains.compose.ui.tooling.preview)
-            implementation(libs.koin.compose)
-            implementation(project.dependencies.platform(libs.koin.bom))
             implementation(projects.core.coreCommon)
             implementation(projects.core.coreNetworkApi)
             implementation(projects.core.corePref)
@@ -41,32 +28,6 @@ kotlin {
             } else {
                 implementation(projects.ui.uiDebugEmpty)
             }
-        }
-
-        commonTest.dependencies {
-            implementation(kotlin("test"))
-            implementation(libs.jetbrains.compose.ui.test)
-            implementation(libs.jetbrains.navigation3.ui)
-        }
-
-        androidMain.dependencies {
-            implementation(libs.androidx.activity.compose)
-            implementation(libs.androidx.datastore.core.okio)
-        }
-
-        androidInstrumentedTest.dependencies {
-            implementation(kotlin("test"))
-        }
-
-        jvmMain.dependencies {
-            implementation(compose.desktop.currentOs)
-            implementation(libs.androidx.datastore.core.okio)
-        }
-
-        iosMain.dependencies {
-        }
-
-        webMain.dependencies {
         }
     }
 }
