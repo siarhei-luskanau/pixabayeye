19c70c21315d0667ff2184ebc5f8d71dacc1ed69
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2026-03-06 07:17:09 +0100

    Migrate Compose screenshot tests to `runComposeUiTest`

--- ui/uiImageDetails/src/androidHostTest/kotlin/siarhei/luskanau/pixabayeye/ui/image/details/ImageDetailsScreenAndroidTest.kt
+++ ui/uiImageDetails/src/androidHostTest/kotlin/siarhei/luskanau/pixabayeye/ui/image/details/ImageDetailsScreenAndroidTest.kt
@@ -1,11 +1,11 @@
 package siarhei.luskanau.pixabayeye.ui.image.details
 
-import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.v2.runComposeUiTest
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.github.takahirom.roborazzi.captureRoboImage
 import kotlin.test.Test
-import org.junit.Rule
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -14,58 +14,56 @@ import siarhei.luskanau.pixabayeye.core.network.api.HitModel
 import siarhei.luskanau.pixabayeye.core.stub.resources.HIT_LIST
 import siarhei.luskanau.pixabayeye.core.stub.resources.TYPES_IMAGE
 
+@OptIn(ExperimentalTestApi::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [36], qualifiers = RobolectricDeviceQualifiers.SmallPhone)
 class ImageDetailsScreenAndroidTest {
 
-    @get:Rule
-    val composeRule = createComposeRule()
-
     private val stubData: HitModel by lazy { HIT_LIST.first { TYPES_IMAGE.contains(it.type) } }
 
     @Test
-    fun lightLoading() {
-        composeRule.setContent { ImageDetailsLoadingContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun lightLoading() = runComposeUiTest {
+        setContent { ImageDetailsLoadingContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 
     @Test
     @Config(qualifiers = "+night")
-    fun nightLoading() {
-        composeRule.setContent { ImageDetailsLoadingContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun nightLoading() = runComposeUiTest {
+        setContent { ImageDetailsLoadingContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 
     @Test
-    fun lightSuccess() {
-        composeRule.setContent { ImageDetailsSuccessContentPreview(hitModel = stubData) }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
-    }
-
-    @Test
-    @Config(qualifiers = "+night")
-    fun nightSuccess() {
-        composeRule.setContent { ImageDetailsSuccessContentPreview(hitModel = stubData) }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
-    }
-
-    @Test
-    fun lightError() {
-        composeRule.setContent { ImageDetailsErrorContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun lightSuccess() = runComposeUiTest {
+        setContent { ImageDetailsSuccessContentPreview(hitModel = stubData) }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 
     @Test
     @Config(qualifiers = "+night")
-    fun nightError() {
-        composeRule.setContent { ImageDetailsErrorContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun nightSuccess() = runComposeUiTest {
+        setContent { ImageDetailsSuccessContentPreview(hitModel = stubData) }
+        waitForIdle()
+        onRoot().captureRoboImage()
+    }
+
+    @Test
+    fun lightError() = runComposeUiTest {
+        setContent { ImageDetailsErrorContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
+    }
+
+    @Test
+    @Config(qualifiers = "+night")
+    fun nightError() = runComposeUiTest {
+        setContent { ImageDetailsErrorContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 }
--- ui/uiVideoDetails/src/androidHostTest/kotlin/siarhei/luskanau/pixabayeye/ui/video/details/VideoDetailsScreenAndroidTest.kt
+++ ui/uiVideoDetails/src/androidHostTest/kotlin/siarhei/luskanau/pixabayeye/ui/video/details/VideoDetailsScreenAndroidTest.kt
@@ -1,11 +1,11 @@
 package siarhei.luskanau.pixabayeye.ui.video.details
 
-import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.v2.runComposeUiTest
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.github.takahirom.roborazzi.captureRoboImage
 import kotlin.test.Test
-import org.junit.Rule
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -14,58 +14,56 @@ import siarhei.luskanau.pixabayeye.core.network.api.HitModel
 import siarhei.luskanau.pixabayeye.core.stub.resources.HIT_LIST
 import siarhei.luskanau.pixabayeye.core.stub.resources.TYPES_VIDEO
 
+@OptIn(ExperimentalTestApi::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [36], qualifiers = RobolectricDeviceQualifiers.SmallPhone)
 class VideoDetailsScreenAndroidTest {
 
-    @get:Rule
-    val composeRule = createComposeRule()
-
     private val stubData: HitModel by lazy { HIT_LIST.first { TYPES_VIDEO.contains(it.type) } }
 
     @Test
-    fun lightLoading() {
-        composeRule.setContent { VideoDetailsLoadingContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun lightLoading() = runComposeUiTest {
+        setContent { VideoDetailsLoadingContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 
     @Test
     @Config(qualifiers = "+night")
-    fun nightLoading() {
-        composeRule.setContent { VideoDetailsLoadingContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun nightLoading() = runComposeUiTest {
+        setContent { VideoDetailsLoadingContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 
     @Test
-    fun lightSuccess() {
-        composeRule.setContent { VideoDetailsSuccessContentPreview(hitModel = stubData) }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
-    }
-
-    @Test
-    @Config(qualifiers = "+night")
-    fun nightSuccess() {
-        composeRule.setContent { VideoDetailsSuccessContentPreview(hitModel = stubData) }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
-    }
-
-    @Test
-    fun lightError() {
-        composeRule.setContent { VideoDetailsErrorContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun lightSuccess() = runComposeUiTest {
+        setContent { VideoDetailsSuccessContentPreview(hitModel = stubData) }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 
     @Test
     @Config(qualifiers = "+night")
-    fun nightError() {
-        composeRule.setContent { VideoDetailsErrorContentPreview() }
-        composeRule.waitForIdle()
-        composeRule.onRoot().captureRoboImage()
+    fun nightSuccess() = runComposeUiTest {
+        setContent { VideoDetailsSuccessContentPreview(hitModel = stubData) }
+        waitForIdle()
+        onRoot().captureRoboImage()
+    }
+
+    @Test
+    fun lightError() = runComposeUiTest {
+        setContent { VideoDetailsErrorContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
+    }
+
+    @Test
+    @Config(qualifiers = "+night")
+    fun nightError() = runComposeUiTest {
+        setContent { VideoDetailsErrorContentPreview() }
+        waitForIdle()
+        onRoot().captureRoboImage()
     }
 }
